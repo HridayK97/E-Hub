6b374d18136900791ddc868da274b39b429ebc0d
HridayK97 <hridayk@gmail.com>
2019-01-18 19:10:00 +0530

    Updated Menu Routes

--- src/views/MainPage.jsx
+++ src/views/MainPage.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import { Redirect, Switch, Router, Route, Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { Layout, Row, Spin, Menu, Breadcrumb, Icon } from 'antd';
 import Responsive from 'react-responsive';
+import MarketView from './MarketPlace/MarketView.jsx';
 import { setUserDetails } from '../reducers/main';
+import mainRoutes from '../routes/mainRoutes';
 import firebase from '../config/config';
 
 const Mobile = props => <Responsive {...props} maxWidth={767} />;
@@ -77,12 +80,14 @@ class MainPage extends React.Component {
           <Menu
             theme="dark"
             mode="horizontal"
-            defaultSelectedKeys={['2']}
+            defaultSelectedKeys={['1']}
             style={{ lineHeight: '64px' }}
           >
-            <Menu.Item key="1">Home</Menu.Item>
-            <Menu.Item key="2">Sell</Menu.Item>
-            <Menu.Item key="3">My Profile</Menu.Item>
+            <Menu.Item key="1">
+              <Link to="/main/market">Home</Link>
+            </Menu.Item>
+            <Menu.Item key="2"><Link to="/main/sell">Sell</Link></Menu.Item>
+            <Menu.Item key="3"><Link to="/main/account">My Account</Link></Menu.Item>
 
             <Menu.Item onClick={this.logout} style={{ float: 'right' }} key="4">
               Logout
@@ -90,65 +95,12 @@ class MainPage extends React.Component {
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Layout style={{ padding: '24px 0', background: '#fff' }}>
-            <Sider width={100} style={{ background: '#fff' }}>
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%' }}
-              >
-                <SubMenu
-                  key="sub1"
-                  title={
-                    <span>
-                      <Icon type="user" />
-                      subnav 1
-                    </span>
-                  }
-                >
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub2"
-                  title={
-                    <span>
-                      <Icon type="laptop" />
-                      subnav 2
-                    </span>
-                  }
-                >
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub3"
-                  title={
-                    <span>
-                      <Icon type="notification" />
-                      subnav 3
-                    </span>
-                  }
-                >
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-              </Menu>
-            </Sider>
-            <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
-          </Layout>
+          <Switch>
+          {mainRoutes.map((route, key) => {
+            if (route.redirect) return <Redirect from={route.path} to={route.pathTo} key={key} />;
+            return <Route path={route.path} component={route.component} />;
+          })}
+          </Switch>
         </Content>
         <Footer style={{ textAlign: 'center' }}>E-Hub Footer</Footer>
       </Layout>
@@ -163,78 +115,27 @@ class MainPage extends React.Component {
           <Menu
             theme="dark"
             mode="horizontal"
-            defaultSelectedKeys={['2']}
+            defaultSelectedKeys={['1']}
             style={{ lineHeight: '64px' }}
           >
-            <Menu.Item key="1">Home</Menu.Item>
-            <Menu.Item key="2">Sell</Menu.Item>
-            <Menu.Item key="3">My Profile</Menu.Item>
+          <Menu.Item key="1">
+          <Link to="/main/market">Home</Link>
+        </Menu.Item>
+        <Menu.Item key="2"><Link to="/main/sell">Sell</Link></Menu.Item>
+        <Menu.Item key="3"><Link to="/main/account">My Account</Link></Menu.Item>
 
             <Menu.Item onClick={this.logout} style={{ float: 'right' }} key="4">
               Logout
             </Menu.Item>
           </Menu>
         </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <Layout style={{ padding: '24px 0', background: '#fff' }}>
-            <Sider width={100} style={{ background: '#fff' }}>
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%' }}
-              >
-                <SubMenu
-                  key="sub1"
-                  title={
-                    <span>
-                      <Icon type="user" />
-                      subnav 1
-                    </span>
-                  }
-                >
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub2"
-                  title={
-                    <span>
-                      <Icon type="laptop" />
-                      subnav 2
-                    </span>
-                  }
-                >
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub3"
-                  title={
-                    <span>
-                      <Icon type="notification" />
-                      subnav 3
-                    </span>
-                  }
-                >
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-              </Menu>
-            </Sider>
-            <Content style={{ padding: '0 24px', minHeight: 280 }}>Content</Content>
-          </Layout>
+        <Content style={{ padding: '0 5px' }}>
+          <Switch>
+            {mainRoutes.map((route, key) => {
+              if (route.redirect) return <Redirect from={route.path} to={route.pathTo} key={key} />;
+              return <Route path={route.path} component={route.component} />;
+            })}
+          </Switch>
         </Content>
         <Footer style={{ textAlign: 'center' }}>E-Hub Footer</Footer>
       </Layout>
