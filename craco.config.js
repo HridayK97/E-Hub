70c3ff436bdc0bad9d21bccd45940aa9325bfc7a
HridayK97 <hridayk@gmail.com>
2019-01-19 16:10:27 +0530

    Updated package.json. Removed eslint dev dependency to remove conflict with react-scripts

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,15 @@
-const CracoAntDesignPlugin = require("craco-antd");
+const CracoAntDesignPlugin = require('craco-antd');
 
 module.exports = {
-  plugins: [{ 
-    plugin: CracoAntDesignPlugin,
-    options: {
-      customizeTheme: {
-        //  "@primary-color": "#FF0000",
-        //  "@link-color": "#FF0000"
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeTheme: {
+          //  "@primary-color": "#FF0000",
+          //  "@link-color": "#FF0000"
+        }
       }
     }
-  }]
+  ]
 };
